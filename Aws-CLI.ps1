c6a9f8afea01f8aba3f3d6b35a134cfb0eda4873
rahulrajvn <rahulrajvn@gmail.com>
2020-03-31 22:32:11 +0400

    Create Aws-CLI.ps1

---
+++ Aws-CLI.ps1
@@ -0,0 +1,18 @@
+
+
+aws ec2 describe-instances --profile adfaws --output text --query 'Reservations[*].Instances[*].[InstanceId, InstanceType, ImageId, State.Name, LaunchTime, Placement.AvailabilityZone, Placement.Tenancy, PrivateIpAddress, PrivateDnsName, PublicDnsName, [Tags[?Key==`Name`].Value] [0][0], [Tags[?Key==`purpose`].Value] [0][0], [Tags[?Key==`environment`].Value] [0][0], [Tags[?Key==`team`].Value] [0][0] ]' > instances.csv
+
+
+
+
+aws ec2 describe-volumes --profile adfaws --output text --query 'Volumes[*].Attachments[].{VolumeID:VolumeId,InstanceID:InstanceId}'
+
+
+
+aws ec2 describe-volumes --profile adfaws  --output text --query 'Volumes[*].{ID:VolumeId,InstanceId:Attachments[0].InstanceId,AZ:AvailabilityZone,Size:Size}' > instancesebs.csv
+
+
+
+
+
+
