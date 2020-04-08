e2594bec30373528e435f53d18e26ac2425f6f3c
rahulrajvn <rahulrajvn@gmail.com>
2020-04-08 21:42:33 +0400

    Update Aws-CLI.ps1

--- Aws-CLI.ps1
+++ Aws-CLI.ps1
@@ -1,6 +1,6 @@
 
 
-aws ec2 describe-instances --profile adfaws --output text --query 'Reservations[*].Instances[*].[InstanceId, InstanceType, ImageId, State.Name, LaunchTime, Placement.AvailabilityZone, Placement.Tenancy, PrivateIpAddress, PrivateDnsName, PublicDnsName, [Tags[?Key==`Name`].Value] [0][0], [Tags[?Key==`purpose`].Value] [0][0], [Tags[?Key==`environment`].Value] [0][0], [Tags[?Key==`team`].Value] [0][0] ]' > instances.csv
+aws ec2 describe-instances --profile mafro --output text --query 'Reservations[*].Instances[*].[InstanceId, InstanceType, ImageId, State.Name, LaunchTime, Placement.AvailabilityZone, Placement.Tenancy, PrivateIpAddress, PrivateDnsName, PublicDnsName, [Tags[?Key==`Name`].Value] [0][0], [Tags[?Key==`purpose`].Value] [0][0], [Tags[?Key==`environment`].Value] [0][0], [Tags[?Key==`team`].Value] [0][0] ]' > instances.csv
 
 
 
@@ -9,7 +9,7 @@ aws ec2 describe-volumes --profile adfaws --output text --query 'Volumes[*].Atta
 
 
 
-aws ec2 describe-volumes --profile adfaws  --output text --query 'Volumes[*].{ID:VolumeId,InstanceId:Attachments[0].InstanceId,AZ:AvailabilityZone,Size:Size}' > instancesebs.csv
+aws ec2 describe-volumes --profile mafro  --output text --query 'Volumes[*].{ID:VolumeId,InstanceId:Attachments[0].InstanceId,AZ:AvailabilityZone,Size:Size}' > instancesebs.csv
 
 
 
